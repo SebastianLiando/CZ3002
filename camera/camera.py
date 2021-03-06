efa7787a0b4caac424fb251f02c2883a6f40cc92
Jason <c170127@e.ntu.edu.sg>
2021-03-06 22:58:45 +0800

    refactor run camera method

--- camera/camera.py
+++ camera/camera.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import time
 
 from face_detection import FaceDetector
@@ -89,73 +90,105 @@ class Camera:
                     skip_scale_branch_list=[],
                 )
 
-                for bbox in bboxes:
-                    confidence = bbox[-1]
-                    bbox = tuple(map(int, bbox[:4]))  # convert to int
-
-                    if bbox[2] <= bbox[0] or bbox[3] <= bbox[1]:
-                        continue  # empty bbox
-
-                    bbox = (
-                        max(bbox[0] - left_padding, 0),
-                        max(bbox[1] - top_padding, 0),
-                        min(bbox[2] + right_padding, frame_width),
-                        min(bbox[3] + bottom_padding, frame_height),
-                    )
-
-                    face = frame[bbox[1]: bbox[3], bbox[0]: bbox[2]]
-                    gender, gender_score = self.gender_classifier.predict(face)
-
-                    if gender != self.toilet_gender:
-                        new_violation_time = time.time()
-
-                        # at least n seconds interval between each notification
-                        if new_violation_time - self.last_violation_time >= self.notification_interval:
-                            self.last_violation_time = new_violation_time
-
-                            print(
-                                f'sending notification regarding violation: '
-                                f'{self.gender_map[gender]} in '
-                                f'{self.gender_map[self.toilet_gender]} toilet'
-                            )
-                            self.notifier.notify(
-                                violation_info={
-                                    'detectedGender': gender,
-                                    'locationGender': self.toilet_gender,
-                                    'location': self.toilet_location,
-                                    'timestamp': new_violation_time,
-                                    'isTrue': True,
-                                    'verifiedBy': None,
-                                },
-                                image=frame,
-                            )
-
-                    if debug:
-                        debug_frame = cv2.rectangle(
-                            frame,
-                            (bbox[0], bbox[1]),  # top-left point
-                            (bbox[2], bbox[3]),  # bottom-right point
-                            color=(0, 255, 0),  # green
-                            thickness=2,
-                        )
-
-                        debug_frame = cv2.putText(
-                            debug_frame,
-                            f'{gender} {confidence:.2f} {gender_score:.2f}',
-                            (bbox[0], bbox[1] + 25),  # top-left point
-                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                            fontScale=1,
-                            color=(0, 255, 0),  # green
-                            thickness=2,
-                        )
-                
-                if debug:
-                    cv2.imshow('camera', debug_frame)
+                self.__handle_bboxes(
+                    debug=debug,
+                    frame=frame,
+                    bboxes=bboxes,
+                    frame_width=frame_width,
+                    frame_height=frame_height,
+                    left_padding=left_padding,
+                    top_padding=top_padding,
+                    right_padding=right_padding,
+                    bottom_padding=bottom_padding,
+                )
 
                 if cv2.waitKey(1) == ord('q'):  # press q to stop
                     break
+            
         except KeyboardInterrupt:
             print('stopping camera...')
         finally:
             video_capture.release()
             cv2.destroyAllWindows()
+    
+    def __handle_bboxes(
+        self,
+        debug: bool,
+        frame: np.ndarray,
+        bboxes: list,
+        frame_width: int,
+        frame_height: int,
+        left_padding: int,
+        top_padding: int,
+        right_padding: int,
+        bottom_padding: int,
+    ):
+        for bbox in bboxes:
+            confidence = bbox[-1]
+            bbox = tuple(map(int, bbox[:4]))  # convert to int
+
+            if bbox[2] <= bbox[0] or bbox[3] <= bbox[1]:
+                continue  # empty bbox
+
+            bbox = (
+                max(bbox[0] - left_padding, 0),
+                max(bbox[1] - top_padding, 0),
+                min(bbox[2] + right_padding, frame_width),
+                min(bbox[3] + bottom_padding, frame_height),
+            )
+
+            face = frame[bbox[1]: bbox[3], bbox[0]: bbox[2]]
+            gender, gender_score = self.gender_classifier.predict(face)
+
+            self.__handle_gender(frame=frame, gender=gender)
+
+            if debug:
+                debug_frame = cv2.rectangle(
+                    frame,
+                    (bbox[0], bbox[1]),  # top-left point
+                    (bbox[2], bbox[3]),  # bottom-right point
+                    color=(0, 255, 0),  # green
+                    thickness=2,
+                )
+
+                debug_frame = cv2.putText(
+                    debug_frame,
+                    f'{gender} {confidence:.2f} {gender_score:.2f}',
+                    (bbox[0], bbox[1] + 25),  # top-left point
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1,
+                    color=(0, 255, 0),  # green
+                    thickness=2,
+                )
+        
+        if debug:
+            cv2.imshow('camera', debug_frame)
+
+    def __handle_gender(
+        self,
+        frame: np.ndarray,
+        gender: int,
+    ):
+        if gender != self.toilet_gender:
+            new_violation_time = time.time()
+
+            # at least n seconds interval between each notification
+            if new_violation_time - self.last_violation_time >= self.notification_interval:
+                self.last_violation_time = new_violation_time
+
+                print(
+                    f'sending notification regarding violation: '
+                    f'{self.gender_map[gender]} in '
+                    f'{self.gender_map[self.toilet_gender]} toilet'
+                )
+                self.notifier.notify(
+                    violation_info={
+                        'detectedGender': gender,
+                        'locationGender': self.toilet_gender,
+                        'location': self.toilet_location,
+                        'timestamp': new_violation_time,
+                        'isTrue': True,
+                        'verifiedBy': None,
+                    },
+                    image=frame,
+                )
